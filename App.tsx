a48a91236294516c7709d4f9e365ccf840848336
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-25 12:33:57 -0300

    Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import theme from './src/theme';
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent 
+      />
       <SignIn />
     </ThemeProvider>
   );
