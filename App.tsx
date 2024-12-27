34dac2ccdfde0fd4e1fe3aaf0c7353f2cb9a1033
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-27 10:22:43 -0300

    Instalando o RealmDB no projeto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import { StatusBar } from 'react-native';
+import { AppProvider, UserProvider } from '@realm/react';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { REALM_APP_ID } from '@env';
+
 import theme from './src/theme';
+
+import { Home } from './src/screens/Home';
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
@@ -19,13 +24,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar 
-        barStyle="light-content" 
-        backgroundColor="transparent" 
-        translucent 
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle="light-content" 
+          backgroundColor="transparent" 
+          translucent 
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
