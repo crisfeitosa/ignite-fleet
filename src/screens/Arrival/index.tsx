e90f3fda035daf5d0cc999c5b9d2e3cb79f88301
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-28 16:40:40 -0300

    Registrando a chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -44,6 +44,25 @@ export function Arrival() {
     goBack();
   };
 
+  function handleArrivalRegister() {
+    try {
+      if(!historic) {
+        return Alert.alert('Erro', 'Não foi possível obter os dados para registrar a chegada do veículo.')
+      }
+
+      realm.write(() => {
+        historic.status = 'arrival';
+        historic.updated_at = new Date();
+      });
+
+      Alert.alert('Chegada', 'Chegada registrada com sucesso.');
+
+      goBack();
+    } catch (error) {
+      Alert.alert('Erro', "Não foi possível registar a chegada do veículo.")
+    }
+  };
+
   return (
     <Container>
       <Header title='Chegada' />
@@ -71,7 +90,10 @@ export function Arrival() {
             onPress={handleRemoveVehicleUsage}
           />
 
-          <Button title='Registrar chegada' />
+          <Button 
+            title='Registrar chegada' 
+            onPress={handleArrivalRegister}
+          /> 
         </Footer>
       </Content>
     </Container>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -36,8 +36,12 @@ export function Home() {
   };
 
   useEffect(() => {
+    fetchVehicleInUse();
+  },[])
+
+  useEffect(() => {
     realm.addListener('change', () => fetchVehicleInUse());
-    
+
     return () => realm.removeListener('change', fetchVehicleInUse);
   },[]);
 
