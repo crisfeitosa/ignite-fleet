8e7c9e25fa5863736c8e4e13183658ef34c9ba2e
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-28 13:31:15 -0300

    Componente Button

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { TextAreaInput } from '../../components/TextAreaInput';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
@@ -18,6 +19,10 @@ export function Departure() {
         <TextAreaInput 
           label='Finalidade'
           placeholder='Vou utilizar o veículo para...'
+        />
+
+        <Button
+          title='Registar Saída'
         />
       </Content>
     </Container>
