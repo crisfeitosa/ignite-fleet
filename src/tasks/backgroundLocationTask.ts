bdadac6877a8a20435ece28f0d2600dd74913d4c
Cristiano Feitosa <cris.immobilis@gmail.com>
2024-12-31 15:32:17 -0300

    Utilizando o saveStorageLocation

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -3,32 +3,40 @@ import {
   hasStartedLocationUpdatesAsync, 
   startLocationUpdatesAsync ,
   stopLocationUpdatesAsync
-} from 'expo-location';
+} from 'expo-location'
 import * as TaskManager from 'expo-task-manager';
+import { saveStorageLocation } from '../libs/asyncStorage/locationStorage';
 
 export const BACKGROUND_TASK_NAME =  'location-tracking';
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
     if(error) {
-      throw error;
+      throw error
     }
 
-    const { coords, timestamp } = data.locations[0];
-    const currentLocation = {
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      timestamp
+    if(data) {
+      const { coords, timestamp } = data.locations[0];
+
+      const currentLocation = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        timestamp
+      }
+
+      await saveStorageLocation(currentLocation)
     }
-    console.log(currentLocation);
+
   } catch (error) {
     console.log(error)
+    stopLocationTask();
   }
 });
 
 export async function startLocationTask() {
   try {
-    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME)
+    const hasStarted = await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME);  
+
     if(hasStarted) {
       await stopLocationTask();
     };
